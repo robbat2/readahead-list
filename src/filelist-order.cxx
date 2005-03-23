69f882b442243ce2c6371b6ed5a3a4b2fc179f4d
robbat2 <robbat2>
2005-03-23 05:58:13 +0000

    fix licenses.

--- src/filelist-order.cxx
+++ src/filelist-order.cxx
@@ -1,3 +1,19 @@
+// Copyright (C) 2005 Robin Hugh Johnson <robbat2@orbis-terrarum.net>
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software 
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
 #include <map>
 #include <set>
 #include <iostream>
@@ -35,8 +51,8 @@ int __STACK_DEBUG__ = 0;
 using namespace std;
 
 static char* program_name = "readhead-list";
-static char* program_header = "$Header: /code/convert/cvsroot/infrastructure/readahead-list/src/filelist-order.cxx,v 1.1 2005/03/23 04:38:19 robbat2 Exp $";
-static char* program_id = "$Id: filelist-order.cxx,v 1.1 2005/03/23 04:38:19 robbat2 Exp $";
+static char* program_header = "$Header: /code/convert/cvsroot/infrastructure/readahead-list/src/filelist-order.cxx,v 1.2 2005/03/23 05:58:13 robbat2 Exp $";
+static char* program_id = "$Id: filelist-order.cxx,v 1.2 2005/03/23 05:58:13 robbat2 Exp $";
 
 static int flag_input_file = 0;
 static int flag_input_stdin = 0;
