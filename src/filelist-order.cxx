37e699689661249c88a020681a70cd0633ee7ef4
Sam James <sam@gentoo.org>
2022-12-25 20:34:38 +0000

    Set -D_GNU_SOURCE for readahead() to fix -Wimplicit-function-declaration
    readahead-list.c: In function ‘process_file’:
    readahead-list.c:111:17: error: implicit declaration of function ‘readahead’ [-Werror=implicit-function-declaration]
      111 |                 readahead(fd, (loff_t)0, (size_t)buf.st_size);
          |                 ^~~~~~~~~
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- src/filelist-order.cxx
+++ src/filelist-order.cxx
@@ -14,6 +14,9 @@
 // along with this program; if not, write to the Free Software 
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
+
+#include <../config.h>
+
 #include <map>
 #include <set>
 #include <iostream>
@@ -31,7 +34,6 @@
 
 #include <fcntl.h>
 #include <linux/fs.h>
-#include <../config.h>
 
 #ifndef BUFFER_SIZE
 #define BUFFER_SIZE 2048
